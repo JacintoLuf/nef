a5b7b8c67ade5d0e82dc75aff99713aaa1ef9574
JacintoLuf <jacinto.luf@ua.pt>
2023-03-28 16:35:54 +0100

    updates

--- dummy-nef/main.py
+++ dummy-nef/main.py
@@ -1,6 +1,7 @@
 from typing import Union
 from fastapi import FastAPI
-from hyper import HTTPConnection
+#from hyper import HTTPConnection
+import httpx
 
 app = FastAPI()
 
@@ -8,6 +9,12 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+@app.get("/test2")
+async def test_conn2():
+    async with httpx.AsyncClient(http2=True, verify=False) as client:
+        response = await client.get("https://10.244.2.42:80//nnrf-nfm/v1/nf-instances", headers={'Accept': 'application/json'})
+    return response.json()
 
 @app.get("/test")
 def test_conn():
