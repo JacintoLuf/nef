bffe2d2faf1507b285b5e0a8beb89005f82543f6
JacintoLuf <jacinto.luf@ua.pt>
2023-03-28 19:20:29 +0100

    updates

--- dummy-nef/main.py
+++ dummy-nef/main.py
@@ -1,6 +1,6 @@
 from typing import Union
 from fastapi import FastAPI
-#from hyper import HTTPConnection
+import ssl
 import httpx
 from httpx._config import SSLConfig
 
@@ -13,13 +13,15 @@ def read_root():
 
 @app.get("/test2")
 async def test_conn2():
-    ssl = SSLConfig(
-        version=ssl.PROTOCOL_TLSv1_2,
-        cipher_suite=ssl.TLS_CIPHERS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
-    )
+    ctx = ssl.create_default_context()
+    ctx.set_ciphers('ECDHE-RSA-AES128-GCM-SHA256')
+    ctx.set_alpn_protocols(['h2'])
 
-    async with httpx.AsyncClient(http2=True, verify=False, ssl=ssl) as client:
-        response = await client.get("https://10.244.2.42:80//nnrf-nfm/v1/nf-instances", headers={'Accept': 'application/json'})
+    async with httpx.AsyncClient(http2=True, verify=False, ssl=ctx) as client:
+        response = await client.get(
+            "https://10.244.2.42:80//nnrf-nfm/v1/nf-instances",
+            headers={'Accept': 'application/json'},
+        )
         print(response.text)
     return response.json()
 
