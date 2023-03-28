3a39040202525df7f48e745dc5c55afd5abea9bf
JacintoLuf <jacinto.luf@ua.pt>
2023-03-28 19:36:08 +0100

    fixes

--- dummy-nef/main.py
+++ dummy-nef/main.py
@@ -13,13 +13,16 @@ def read_root():
 
 @app.get("/test2")
 async def test_conn2():
-    ctx = ssl.create_default_context()
-    ctx.set_ciphers('ECDHE-RSA-AES128-GCM-SHA256')
-    ctx.set_alpn_protocols(['h2'])
+    ctx = SSLConfig().with_ssl_context(
+        ssl.create_default_context()
+    )
+    ctx.ssl_version = ssl.PROTOCOL_TLSv1_2
+    ctx.ciphers = "ECDHE-RSA-AES128-GCM-SHA256"
+    ctx.alpn_protocols = ["h2"]
 
     async with httpx.AsyncClient(http2=True, verify=False, ssl=ctx) as client:
         response = await client.get(
-            "https://10.244.2.42:80//nnrf-nfm/v1/nf-instances",
+            "https://10.244.2.42/nnrf-nfm/v1/nf-instances",
             headers={'Accept': 'application/json'},
         )
         print(response.text)
