1996842550f0aa923564756d082a58869405ce34
JacintoLuf <jacinto.luf@ua.pt>
2023-03-29 17:38:43 +0100

    Update main.py

--- dummy-nef/main.py
+++ dummy-nef/main.py
@@ -13,13 +13,13 @@ def read_root():
 
 @app.get("/test2")
 async def test_conn2():
-    ssl = SSLConfig(
+    ctx = SSLConfig(
         version=ssl.PROTOCOL_TLSv1_2,
         cipher_suite=ssl.TLS_CIPHERS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
         alpn_protocols=["h2"]
     )
 
-    async with httpx.AsyncClient(http2=True, verify=False, ssl=ssl) as client:
+    async with httpx.AsyncClient(http2=True, verify=False, ssl=ctx) as client:
         response = await client.get(
             "https://10.244.2.52/nnrf-nfm/v1/nf-instances",
             headers={'Accept': 'application/json,application/problem+json'},
