b623eec0e076e6d033fa60930c4de7e040a5132c
JacintoLuf <jacinto.luf@ua.pt>
2023-03-30 16:40:28 +0100

    fixes

--- dummy-nef/main.py
+++ dummy-nef/main.py
@@ -12,7 +12,7 @@ def read_root():
 @app.get("/test")
 async def test_conn():
 
-    async with httpx.AsyncClient(http2=True) as client:
+    async with httpx.AsyncClient(http1=False, http2=True) as client:
         response = await client.get(
             "http://10.244.2.53:80/nnrf-nfm/v1/nf-instances",
             headers={'Accept': 'application/json'}
