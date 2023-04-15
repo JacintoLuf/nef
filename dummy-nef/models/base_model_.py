eab2df28793ee65c4bb98e51f76d88d458f188bb
JacintoLuf <jacinto.luf@ua.pt>
2023-04-15 18:28:13 +0100

    Update base_model_.py

--- dummy-nef/models/base_model_.py
+++ dummy-nef/models/base_model_.py
@@ -33,22 +33,22 @@ class Model(object):
             value = getattr(self, attr)
             if isinstance(value, list):
                 #self.attribute_map[attr]
-                result[attr] = list(map(
+                result[self.attribute_map[attr]] = list(map(
                     lambda x: x.to_dict() if hasattr(x, "to_dict") else x,
                     value
                 ))
             elif hasattr(value, "to_dict"):
-                result[attr] = value.to_dict()
+                result[self.attribute_map[attr]] = value.to_dict()
             elif isinstance(value, dict):
-                result[attr] = dict(map(
+                result[self.attribute_map[attr]] = dict(map(
                     lambda item: (item[0], item[1].to_dict())
                     if hasattr(item[1], "to_dict") else item,
                     value.items()
                 ))
-            # elif isinstance(None):
-            #     pass
+            elif isinstance(None):
+                pass
             else:
-                result[attr] = value
+                result[self.attribute_map[attr]] = value
 
         return result
 
