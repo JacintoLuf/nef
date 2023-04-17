d1b253c837004f23f4666b04b044577101417fd8
JacintoLuf <jacinto.luf@ua.pt>
2023-04-17 09:26:13 +0100

    Update amf_create_event_subscription.py

--- dummy-nef/models/amf_create_event_subscription.py
+++ dummy-nef/models/amf_create_event_subscription.py
@@ -34,7 +34,7 @@ class AmfCreateEventSubscription(Model):
         }
 
         self.attribute_map = {
-            'subscription': 'amfEventSubscription',
+            'subscription': 'AmfEventSubscription',
             'supported_features': 'supportedFeatures',
             'old_guami': 'oldGuami'
         }
