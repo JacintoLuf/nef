9c27e47c2c796c061d42e7935fa40447a8e7abac
JacintoLuf <jacinto.luf@ua.pt>
2023-05-25 11:33:25 +0100

    changes

--- dummy-nef/run.sh
+++ dummy-nef/run.sh
@@ -3,8 +3,8 @@
 # Delete previous existing deployment and service
 kubectl delete deployment -n open5gs nef
 kubectl delete service -n open5gs nef
-kubectl delete clusterrole nef-services-read
-kubectl delete clusterrolebinding nef-services-read-rb
+kubectl delete clusterrole nef-pods-list
+kubectl delete clusterrolebinding nef-pods-list
 kubectl delete serviceaccount -n open5gs nef-account
 
 # Find and delete the Docker image by name
