adecf57b06d995eabf851552341b18d9185e3a05
JacintoLuf <jacinto.luf@ua.pt>
2023-05-30 16:56:26 +0100

    nrf_handler heart beat

--- dummy-nef/core/nrf_handler.py
+++ dummy-nef/core/nrf_handler.py
@@ -1,13 +1,12 @@
 import httpx
 import json
-import uuid
 from api.config import conf
 from session import db
 from models.nf_profile import NFProfile
 from crud import nfProfile
 
 
-async def nrf_discovery() -> str:
+async def nrf_discovery() -> int:
     collection = db["nf_instances"]
     uuids = []
     instances = []
@@ -23,6 +22,9 @@ async def nrf_discovery() -> str:
         j = json.loads(response.text)
         uuids = [i["href"].split('/')[-1] for i in j["_links"]["items"]]
 
+        if response.status_code is not httpx.codes.OK:
+            return response.status_code
+
     async with httpx.AsyncClient(http1=False, http2=True) as client:
         for id in uuids:
             response = await client.get(
@@ -36,17 +38,11 @@ async def nrf_discovery() -> str:
     result = collection.insert_many(instances)
     result.inserted_ids
     print(instances[0])
-    #print(collection.count_documents())
-    return "NF profiles loaded"
 
-async def nf_register() -> str:
-    print(json.dumps(conf.NEF_PROFILE.to_dict()))
+    return response.status_code
+
+async def nf_register() -> int:
     nef_profile = conf.NEF_PROFILE.to_dict()
-    nef_profile.pop('nfServicePersistence')
-    nef_profile.pop('nfProfileChangesSupportInd')
-    nef_profile.pop('nfProfileChangesInd')
-    nef_profile.pop('lcHSupportInd')
-    nef_profile.pop('olcHSupportInd')
     print(nef_profile)
 
     async with httpx.AsyncClient(http1=False, http2=True) as client:
@@ -54,15 +50,48 @@ async def nf_register() -> str:
             "http://"+conf.HOSTS["NRF"][0]+":7777/nnrf-nfm/v1/nf-instances/"+conf.NEF_PROFILE.nf_instance_id,
             headers={
                 'Accept': 'application/json,application/problem+json',
-                'Content-Encoding': 'string',
                 'Content-Type': 'application/json'
                 },
-            data = json.dumps(nef_profile)
+            data = json.dumps(conf.NEF_PROFILE.to_dict())
         )
+        print(response.status_code)
         print(response.text)
 
-    return response.text
+    return response.status_code
 
-def nf_deregister():
+async def nf_deregister():
+    async with httpx.AsyncClient(http1=False, http2=True) as client:
+        response = await client.delete(
+            "http://"+conf.HOSTS["NRF"][0]+":7777/nnrf-nfm/v1/nf-instances/"+conf.NEF_PROFILE.nf_instance_id,
+            headers={'Accept': 'application/json,application/problem+json'}
+        )
+        print(response.status_code)
+        print(response.text)
+        if response.status_code == httpx.codes.NO_CONTENT:
+            print("Deregistered!")
+        if response.status_code == httpx.codes.NOT_FOUND:
+            print("NEF instance not registered")
 
-    return None
+    return response.status_code
+
+async def nf_register_heart_beat():
+
+    async with httpx.AsyncClient(http1=False, http2=True) as client:
+        response = await client.patch(
+            "http://"+conf.HOSTS["NRF"][0]+":7777/nnrf-nfm/v1/nf-instances/"+conf.NEF_PROFILE.nf_instance_id,
+            headers={
+                'Accept': 'application/json,application/problem+json',
+                'Content-Type': 'application/json-patch+json'
+                },
+            data = json.dumps([{ "op": "replace", "path": "/nfStatus", "value": "REGISTERED" }])
+        )
+        print(response.status_code)
+        print(response.text)
+        if response.status_code == httpx.codes.OK:
+            new_nef_profile = NFProfile.from_dict(response.json())
+            print(f"new profile {json.dumps(new_nef_profile)}")
+        elif response.status_code == httpx.codes.NO_CONTENT:
+            print("NRF Heart-Beat!")
+        elif response.status_code == httpx.codes.NOT_FOUND:
+            print("NEF instance not registered")
+    return 
--- dummy-nef/main.py
+++ dummy-nef/main.py
@@ -1,15 +1,8 @@
-from fastapi import FastAPI, Response
-from session import async_db, close
+from fastapi import FastAPI
+from fastapi_utils.tasks import repeat_every
+from session import async_db
 import httpx
 import logging
-import json
-from models.nf_profile import NFProfile
-from models.amf_create_event_subscription import AmfCreateEventSubscription
-from models.amf_event_subscription import AmfEventSubscription
-from models.amf_event import AmfEvent
-from enums.amf_event_type import AmfEventType
-from models.subscription_data import SubscriptionData
-import uuid
 from api.config import conf
 import core.nrf_handler as nrf_handler
 
@@ -20,128 +13,28 @@ logger = logging.getLogger(__name__)
 @app.on_event("startup")
 async def startup():
     print("starting up")
-    
     res = await nrf_handler.nrf_discovery()
     print(res)
     res = await nrf_handler.nf_register()
-    print(res)
-        
+    if res == httpx.codes.CREATED:
+        nrf_heartbeat()  
     print("started")
+
+@repeat_every(seconds=conf.NEF_PROFILE.heart_beat_timer - 5)
+async def nrf_heartbeat():
+    nrf_handler.nf_register_heart_beat()
     
 @app.on_event("shutdown")
 async def shutdown():
-    print("Database reset complete.")
-    close()
+    print("shuting down...")
+    nrf_handler.nf_deregister()
 
 @app.get("/")
 async def read_root():
-    return {"Hello": "World"}
-
-@app.get("/core")
-async def get_core_nf():
     collection = async_db["nf_instances"]
     insts = []
     async for user in collection.find({}):
         insts.append(user)
     return {'nfs instances': str(insts)}
 
-@app.get("amf-status")
-async def amf_comm():
-    sub = SubscriptionData("http://10.102.141.12:80/amf-status-callback", str(uuid.uuid4()), subscription_id="1")
-    print(json.dumps(sub))
-    async with httpx.AsyncClient(http1=False, http2=True) as client:
-        response = await client.post(
-            "http://"+conf.AMF_IP+"/namf-comm/v1/subscriptions",
-            headers={
-                'Accept': 'application/json,application/problem+json',
-                'Content-Type': 'application/json'
-            },
-            data = json.dumps(sub)
-        )
-        print(response.text)
-    return response.text
 
-@app.post("/amf-status-callback")
-async def amf_status_callback(data: dict):
-    try:
-        keys = []
-        for key in data.keys():
-            keys.append(key)
-        print(keys)
-    except:
-        None
-    print(str(data))
-    sub = SubscriptionData.from_dict(data)
-    print(sub.to_str)
-    return Response(status_code=204)
-
-@app.get("nrf-register")
-async def nrf_register():
-    nef_profile = conf.NEF_PROFILE
-    async with httpx.AsyncClient(http1=False, http2=True) as client:
-        response = await client.put(
-            "http://"+nrf+"/nnrf-nfm/v1/nf-instances/"+conf.API_UUID,
-            headers={
-                'Accept': 'application/json,application/problem+json',
-                'Content-Type': 'application/json'
-            },
-            data = json.dumps(nef_profile)
-        )
-    print(response.text)
-    return response.json()
-
-@app.get("nrf-register-callback")
-async def nrf_register_callback():
-
-    return None
-
-@app.get("smf-pdu-session")
-async def smf_policy_control():
-    #/npcf-/v1
-    return None
-
-@app.get("/ip/{nf_type}")
-async def get_nf_ip(nf_type: str):
-    async with httpx.AsyncClient(http1=False, http2=True) as client:
-        response = await client.get(
-            "http://"+nrf+"/nnrf-disc/v1/nf-instances",
-            headers={'Accept': 'application/json,application/problem+json'},
-            params= {"target-nf-type": f"{nf_type.upper()}", "requester-nf-type": "NEF"}
-        )
-    print(response.text)
-    js = response.json()["nfInstances"]
-    profiles = []
-    for item in js:
-        profiles.append(NFProfile.from_dict(item))
-    print("deserialized")
-    return {"nf instance id" : [pf.nf_instance_id for pf in profiles], "ipv4 address": [pf.ipv4_addresses for pf in profiles]}
-
-@app.get("/amf-sub")
-async def test_amf():
-    event = AmfEvent(type=AmfEventType.CONNECTIVITY_STATE_REPORT)
-    sub = AmfEventSubscription([event], "http://10.102.141.12:80/amf-sub-res", "1", str(uuid.uuid4()), any_ue=True)
-    create = AmfCreateEventSubscription(sub)
-    create_event = {"AmfCreateEventSubscription": create.to_dict()}
-    print("--------------------------------")
-    print(json.dumps(create_event))
-    async with httpx.AsyncClient(http1=False, http2=True) as client:
-        response = await client.post(
-            "http://"+conf.AMF_IP+"/namf-comm/v1/subscriptions",
-            headers={
-                'Accept': 'application/json,application/problem+json',
-                'Content-Type': 'application/json'
-            },
-            data = json.dumps(create_event)
-        )
-        print(response.text)
-    return response.text
-
-@app.get("/nf-register")
-async def register_nf():
-    async with httpx.AsyncClient(http1=False, http2=True) as client:
-        response = await client.put(
-            "http://"+nrf+"/nnrf-nfm/v1/nf-instances/"+conf.nef_profile.nf_instance_id,
-            headers={'Accept': 'application/json,application/problem+json'}
-        )
-        print(response.text)
-    return response.text
