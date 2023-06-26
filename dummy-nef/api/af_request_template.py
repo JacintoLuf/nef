bad82ccd5848ccf8404eba252967a2c2ae653c96
JacintoLuf <jacinto.luf@ua.pt>
2023-06-26 10:16:58 +0100

    Update af_request_template.py

--- dummy-nef/api/af_request_template.py
+++ dummy-nef/api/af_request_template.py
@@ -4,13 +4,13 @@ from models.route_to_location import RouteToLocation
 from models.route_information import RouteInformation
 
 route_info = RouteInformation(
-    ipv4_addr="",
+    ipv4_addr="10.255.32.132",
     ipv6_addr="",
     port_number="80"
 )
 
 route_to_loc = RouteToLocation(
-    dnai="111111-01",
+    dnai="1-111111",
     route_info=route_info
 )
 
@@ -19,13 +19,13 @@ traffic_influ = TrafficInfluSub(
     #af_app_id="",
     af_trans_id="1",
     dnn="internet",
-    snssai="111111",
+    snssai={"sst": 1, "sd": "111111"},
     any_ue_ind=False,
     subscribed_events="UP_PATH_CHANGE",
-    ipv4_addr="10.45.0.3",
+    ipv4_addr="10.45.0.2",
     notification_destination=f"http://{conf.HOSTS['NEF'][0]}:80/pcf-policy-authorization-callback",
     traffic_filters="",#-----------------------------------------
-    traffic_routes=route_to_loc,
+    traffic_routes=[route_to_loc],
     temp_validities="",
     supp_feat="InfluenceOnTrafficRouting"
 
