907a066eabc64da071e693eb8ed13fbddd40cd37
JacintoLuf <jacinto.luf@ua.pt>
2023-06-27 18:20:28 +0100

    Update pcf_handler.py

--- dummy-nef/core/pcf_handler.py
+++ dummy-nef/core/pcf_handler.py
@@ -21,7 +21,7 @@ async def pcf_policy_authorization_get(app_session_id: str=None):
         return response.json()
 
 async def pcf_policy_authorization_create(binding: PcfBinding=None, traffic_influ_sub: TrafficInfluSub=None):
-    host_addr = binding.pcf_ip_end_points[0].ipv4_address or conf.HOSTS['PCF'][0]
+    host_addr = conf.HOSTS['PCF'][0] #binding.pcf_ip_end_points[0].ipv4_address or
 
     req_data = AppSessionContextReqData(slice_info=traffic_influ_sub.snssai)
     for attr_name in traffic_influ_sub.attribute_map.keys():
