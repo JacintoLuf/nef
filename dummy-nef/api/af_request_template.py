d1adae20cd7abca0f28efd48608ce524e8644806
JacintoLuf <jacinto.luf@ua.pt>
2023-07-03 17:21:17 +0100

    Update af_request_template.py

--- dummy-nef/api/af_request_template.py
+++ dummy-nef/api/af_request_template.py
@@ -8,7 +8,7 @@ from models.snssai import Snssai
 def create_sub():
     snssai = Snssai(sst=1, sd="0x111111")
     route_info = RouteInformation(ipv4_addr="10.255.32.132", port_number=80)
-    route_to_loc = RouteToLocation(dnai="1-111111", route_info=route_info)
+    route_to_loc = RouteToLocation(dnai="internet", route_info=route_info)
     #temp_val = TemporalValidity(str(datetime.now()), str(datetime.now()+timedelta(minutes=10)))
 
     traffic_influ = TrafficInfluSub(
