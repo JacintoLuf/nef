921db532a510a84a2dbc02e4c2bce3a387a4eb52
JacintoLuf <jacinto.luf@ua.pt>
2023-07-14 16:51:24 +0100

    Update config.py

--- dummy-nef/api/config.py
+++ dummy-nef/api/config.py
@@ -69,7 +69,7 @@ class Settings():
             nf_instance_id=self.API_UUID,
             nf_type="NEF",
             nf_status="REGISTERED",
-            heart_beat_timer=60,
+            heart_beat_timer=3600,
             ipv4_addresses=self.HOSTS["NEF"]
         )
 
