5b2c293ae6dadea34b5bd33e32c40e47af361560
JacintoLuf <jacinto.luf@ua.pt>
2023-07-24 16:29:56 +0100

    Update af_request_template.py

--- dummy-nef/api/af_request_template.py
+++ dummy-nef/api/af_request_template.py
@@ -9,7 +9,7 @@ from models.snssai import Snssai
 def create_sub():
     route_info = RouteInformation(ipv4_addr="10.255.32.132", port_number=80)
     route_to_loc = RouteToLocation(dnai="internet", route_info=route_info)
-    flow_infu = FlowInfo(flow_id=10, flow_descriptions=["permit out 17 from 10.45.0.2 to 10.255.32.132 80"])
+    flow_infu = FlowInfo(flow_id=10, flow_descriptions=["permit out 17 from 10.45.0.2 to 10.255.32.132 80", "permit in 17 from 10.255.32.132 to 10.45.0.2"])
 
     traffic_influ = TrafficInfluSub(
         #af_service_id="24caa907-f1ba-4e29-8a78-f9728dd45d83",
@@ -19,7 +19,7 @@ def create_sub():
         dnn="internet",
         any_ue_ind=False,
         subscribed_events="UP_PATH_CHANGE",
-        ipv4_addr="10.45.0.2",
+        ipv4_addr="10.45.0.4",
         notification_destination=f"http://{conf.HOSTS['NEF'][0]}:80/pcf-policy-authorization-callback",
         traffic_filters=[flow_infu],
         request_test_notification=True,
