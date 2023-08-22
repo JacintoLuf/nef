8fce1fcb71d2b711062dcd290f1b11e4a74c58a8
JacintoLuf <jacinto.luf@ua.pt>
2023-08-22 15:08:39 +0100

    Update main.py

--- dummy-nef/main.py
+++ dummy-nef/main.py
@@ -226,10 +226,12 @@ async def qos_get(scsAsId: str, subscriptionId: str=None):
         raise HTTPException(status_code=httpx.codes.NOT_FOUND, detail="content not found")
     return Response(content=res, status_code=httpx.codes.OK)
 
-#post
-app.get("/3gpp-as-session-with-qos/v1/{scsAsId}/subscriptions")
-async def qos_create(scsAsId: str, data: Request):
-    qos_sub: AsSessionWithQoSSubscription = qos_subscription
+
+# @app.post("/3gpp-as-session-with-qos/v1/{scsAsId}/subscriptions")
+# async def qos_create(scsAsId: str, data: Request):
+@app.get("/qos")
+async def qos_create():
+    qos_sub: AsSessionWithQoSSubscription = qos_subscription #data
     if not ((qos_sub.ue_ipv4_addr is not None)^(qos_sub.ue_ipv6_addr is not None)^(qos_sub.mac_addr is not None)):
         raise HTTPException(httpx.codes.BAD_REQUEST, detail="Only one of ipv4Addr, ipv6Addr or macAddr")
     if not ((qos_sub.flow_info is not None)^(qos_sub.eth_flow_info is not None)^(qos_sub.exter_app_id is not None)):
