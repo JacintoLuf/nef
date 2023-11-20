3062bb1ffcc4c55bdc2c94093fca1818944c011a
JacintoLuf <jacinto.luf@ua.pt>
2023-11-20 17:10:27 +0000

    Update config.py

--- dummy-nef/api/config.py
+++ dummy-nef/api/config.py
@@ -48,7 +48,7 @@ class Settings():
 
             # namespace = "open5gs"
             # mongodb_svc_name = "open5gs-mongodb"
-            # nef_svc_name = "nef"
+            nef_svc_name = "nef"
             # nrf_svc_name = "open5gs-nrf-sbi"
 
             # # Get mongodb service ip
@@ -56,9 +56,9 @@ class Settings():
             # self.HOSTS["MONGODB"] = svc.spec.cluster_ip
             # self.MONGO_IP = svc.spec.cluster_ip
             # self.MONGO_URI = "mongodb://"+svc.spec.cluster_ip+"/nef"
-            # # Get nef service ip
-            # svc = v1.read_namespaced_service(nef_svc_name, namespace)
-            # self.HOSTS["NEF"] = [svc.spec.cluster_ip]
+            # Get nef service ip
+            svc = v1.read_namespaced_service(nef_svc_name, self.NAMESPACE)
+            self.HOSTS["NEF"] = [svc.spec.cluster_ip]
             # # Get nrf service ip
             # svc = v1.read_namespaced_service(nrf_svc_name, namespace)
             # self.HOSTS["NRF"] = [svc.spec.cluster_ip]
