670157e3fe0e9cd5e5f7f1e1c230f7f6d6e484f3
JacintoLuf <jacinto.luf@ua.pt>
2023-11-21 15:47:08 +0000

    Update nrf_handler.py

--- dummy-nef/core/nrf_handler.py
+++ dummy-nef/core/nrf_handler.py
@@ -27,7 +27,7 @@ async def nrf_discovery():
         print(response.json())
         if response.json():
             r = response.json() #json.loads(response.text)
-            hrefs += [item["href"] for item in r["_links"]["items"] if r["_links"]["items"]]
+            hrefs += [item["href"] for item in r["_links"]["items"] if r["_links"].get("items")]
     for href in hrefs:
         async with httpx.AsyncClient(http1=False, http2=True) as client:
             response = await client.get(
