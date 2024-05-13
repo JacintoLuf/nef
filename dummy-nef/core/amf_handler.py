b36b64623fca9cdf485794c835b045551eddc623
JacintoLuf <jacinto.luf@ua.pt>
2024-05-13 15:32:46 +0100

    update

--- dummy-nef/core/amf_handler.py
+++ dummy-nef/core/amf_handler.py
@@ -22,14 +22,15 @@ async def amf_event_exposure_subscribe():
         any_ue=True
         )
     amf_evt_sub = AmfCreateEventSubscription(subscription=amf_sub)
-    async with httpx.AsyncClient(http1=True if conf.CORE=="free5gc" else False, http2=None if conf.CORE=="free5gc" else True) as client:
-        response = await client.post(
-            f"http://{conf.HOSTS['PCF'][0]}/namf-evts/v1/subscriptions",
-            headers=conf.GLOBAL_HEADERS,
-            data=json.dumps(amf_evt_sub.to_dict())
-        )
-        print(response.text)
-    return response.json()
+    print(amf_evt_sub.to_dict())
+    # async with httpx.AsyncClient(http1=True if conf.CORE=="free5gc" else False, http2=None if conf.CORE=="free5gc" else True) as client:
+    #     response = await client.post(
+    #         f"http://{conf.HOSTS['PCF'][0]}/namf-evts/v1/subscriptions",
+    #         headers=conf.GLOBAL_HEADERS,
+    #         data=json.dumps(amf_evt_sub.to_dict())
+    #     )
+    #     print(response.text)
+    # return response.json()
 
 async def amf_event_exposure_subscription_update(app_session_id: str=None):
     if app_session_id:
