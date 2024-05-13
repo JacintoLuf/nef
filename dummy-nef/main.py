ab65fd961bcb5084c20fb1809e21dc9d0b659c36
JacintoLuf <jacinto.luf@ua.pt>
2024-05-13 12:42:55 +0100

    Update main.py

--- dummy-nef/main.py
+++ dummy-nef/main.py
@@ -42,10 +42,10 @@ async def startup():
         await nrf_handler.nrf_discovery()
         print("NF status subscribe...")
         await status_subscribe()
-        print("amf UE event subscription")
-        res = await amf_handler.amf_event_exposure_subscribe()
-        # print("udm UE event subscription")
-        # res = await udm_handler.udm_ee_subscription_create()
+        # print("amf UE event subscription")
+        # res = await amf_handler.amf_event_exposure_subscribe()
+        print("udm UE event subscription")
+        res = await udm_handler.udm_ee_subscription_create()
     except Exception as e:
         print(f"Error starting up: {e}")
     # TLS dependant
