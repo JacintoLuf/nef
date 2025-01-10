edfd77ce05e80118cbfd2aa7a4bbf6d35371d61c
JacintoLuf <jacinto.luf@ua.pt>
2025-01-10 16:53:21 +0000

    Update nrf_handler.py

--- core/nrf_handler.py
+++ core/nrf_handler.py
@@ -83,7 +83,6 @@ async def nrf_get_access_token():
     return response.status_code
 
 async def nf_register():
-    conf.logger.info(f"registering nef profile\n{conf.NEF_PROFILE}")
     async with httpx.AsyncClient(http1=True if conf.CORE=="free5gc" else False, http2=None if conf.CORE=="free5gc" else True) as client:
         response = await client.put(
             f"http://{conf.HOSTS['NRF'][0]}/nnrf-nfm/v1/nf-instances/"+conf.NEF_PROFILE.nf_instance_id,
